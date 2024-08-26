f133238695f9da842680f3bb63ada8d53c9dbf7a
agile8118 <agile.8272@gmail.com>
2024-08-26 10:56:29 -0400

    Adds tests for the middleware logic

---
+++ test/middleware.test.js
@@ -0,0 +1,68 @@
+import assert from "node:assert";
+import supertest from "supertest";
+import cpeak from "../lib/index.js";
+
+const PORT = 7543;
+const request = supertest(`http://localhost:${PORT}`);
+
+describe("Middleware functions", function () {
+  let server;
+
+  before(function (done) {
+    server = new cpeak();
+
+    server.beforeEach((req, res, next) => {
+      const value = req.params.value;
+
+      if (value === "random")
+        return res.status(400).json({ error: "an error msg" });
+
+      next();
+    });
+
+    server.beforeEach((req, res, next) => {
+      req.foo = "text";
+      next();
+    });
+
+    server.beforeEach((req, res, next) => {
+      res.unauthorized = () => {
+        res.statusCode = 401;
+        return res;
+      };
+      next();
+    });
+
+    server.route("get", "/bar", (req, res) => {
+      res.status(200).json({ message: req.foo });
+    });
+
+    server.route("get", "/bar-more", (req, res) => {
+      res.unauthorized().json({});
+    });
+
+    server.listen(PORT, done);
+  });
+
+  after(function (done) {
+    server.close(done);
+  });
+
+  it("should modify the req object with a new property", async function () {
+    const res = await request.get("/bar");
+    assert.strictEqual(res.status, 200);
+    assert.strictEqual(res.body.message, "text");
+  });
+
+  it("should modify the res object with a new method", async function () {
+    const res = await request.get("/bar-more");
+    assert.strictEqual(res.status, 401);
+  });
+
+  it("should exit the middleware and route chain if a middleware wants to", async function () {
+    const res = await request.get("/bar?value=random");
+    assert.strictEqual(res.status, 400);
+    assert.strictEqual(res.body.message, undefined);
+    assert.deepStrictEqual(res.body, { error: "an error msg" });
+  });
+});
